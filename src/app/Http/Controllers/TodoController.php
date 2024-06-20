fa038a1e1a23dd7d04ebd15efaefe6f0861be40b
atsuikeda <ike.ke.no.ke@gmail.com>
2024-06-20 15:41:44 +0900

    feat: TodoModelの作成し、TodoControllerで全件取得。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+
+        dd($todoList);
+
         return view('todo.index');
     }
 }
